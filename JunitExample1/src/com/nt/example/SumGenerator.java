2ff7bf27e72edfa19c09353218b44a56dc1774d2
saikiran <aakulasaikiran>
2018-07-17 12:15:25 +0530

    first1

---
+++ JunitExample1/src/com/nt/example/SumGenerator.java
@@ -0,0 +1,11 @@
+package com.nt.example;
+public class SumGenerator {
+public static int calculateSumGenerator(int num1,int num2) {
+
+	
+	return num1+num2;
+}
+	
+	
+	
+}
---
+++ JunitExample1/src/com/nt/example/TestCaseAdd.java
@@ -0,0 +1,17 @@
+package com.nt.example;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TestCaseAdd {
+    @Test
+	public void testAdd() {
+		int excepted=5;
+		assertEquals(excepted,SumGenerator.calculateSumGenerator(2, 3));
+		//assertEquals(excepted,SumGenerator.calculateSumGenerator(3, 3));
+		
+	}
+	
+	
+}
