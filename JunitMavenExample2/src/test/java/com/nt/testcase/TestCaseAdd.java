6f093e9617730cb51b25d5b6beeb4b412fcce42f
saikiran <aakulasaikiran>
2018-07-17 12:25:42 +0530

    commit7

---
+++ JunitMavenExample2/src/test/java/com/nt/testcase/TestCaseAdd.java
@@ -0,0 +1,18 @@
+package com.nt.testcase;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.nt.example.SumGenerator;
+
+public class TestCaseAdd {
+	@Test
+	public void testAdd() {
+		int excepted = 5;
+		assertEquals(excepted, SumGenerator.calculateSumGenerator(2, 3));
+		// assertEquals(excepted,SumGenerator.calculateSumGenerator(3, 3));
+
+	}
+
+}
---
+++ JunitMavenExample2/src/test/java/com/nt/testrunner/TestRunnerExample.java
@@ -0,0 +1,21 @@
+package com.nt.testrunner;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import com.nt.testcase.TestCaseAdd;
+
+public class TestRunnerExample {
+
+	public static void main(String[] args) {
+		Result result = JUnitCore.runClasses(TestCaseAdd.class);
+		System.out.println("Result:" + result.wasSuccessful());
+
+		for (Failure failure : result.getFailures()) {
+			System.out.println(failure.toString());
+		}
+
+	}
+
+}
