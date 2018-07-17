53580e7bfd81e9c01b01693dbb890766396ff948
saikiran <aakulasaikiran>
2018-07-17 12:20:22 +0530

    commit4

---
+++ JunitExampleIgnoreCase/src/com/nt/testrunner/TestRunner.java
@@ -0,0 +1,22 @@
+package com.nt.testrunner;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import com.nt.example.IgnoreExample;
+
+public class TestRunner {
+
+	public static void main(String[] args) {
+	
+		Result result=JUnitCore.runClasses(IgnoreExample.class);
+		
+		for(Failure failure:result.getFailures()) {
+			System.out.println(failure.toString());
+		}
+		
+		System.out.println(result.wasSuccessful());
+	}
+
+}
